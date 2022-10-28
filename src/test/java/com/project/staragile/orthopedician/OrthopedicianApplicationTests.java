891d4ba9effb3e408b528f647e5373c4df7cc61b
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2022-10-28 08:16:31 +0530

    testcases added

--- src/test/java/com/project/staragile/orthopedician/OrthopedicianApplicationTests.java
+++ src/test/java/com/project/staragile/orthopedician/OrthopedicianApplicationTests.java
@@ -1,14 +1,23 @@
 package com.project.staragile.orthopedician;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrthopedicianApplicationTests {
 
+	@Autowired
+	DoctorService doctorService;
 	
 	@Test
 	void contextLoads() {
 	}
 	
+	@Test
+	void testService() {
+	  assertEquals(doctorService.sayHello(), "Hello from Doctor Shubham");
+	}
 }
